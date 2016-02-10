1aae1c1762063ba79093df522c3f1c9ec5321794
micahcourey <micahjcourey@gmail.com>
2016-02-09 20:26:53 -0800

    Refactor code in ES6 and JSX

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,12 @@
 module.exports = {
-  entry: "./public/js/app.js",
+  entry: "./js/app.js",
   output: {
     path: __dirname + "/public",
     filename: "bundle.js"
+  }
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader' }
+    ]
   }
 }
