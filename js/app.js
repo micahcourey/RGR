53bb4874254a1e25f53e47e6241e716f8ef2b078
micahcourey <micahjcourey@gmail.com>
2016-02-13 19:24:14 -0800

    Create dispatcher actions

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-import Hello from "./components/Hello"
+import Main from "./components/Main"
 
-ReactDom.render(<Hello />, document.getElementById('react'));
+ReactDom.render(<Main />, document.getElementById('react'));
--- js/components/Hello.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default class Hello extends React.Component {
-  render() {
-    return <h1>Hi React with ES6</h1>;
-  }
-}
