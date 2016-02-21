959f8feaea5372fad8c069ac84547dd8205f2725
micahcourey <micahjcourey@gmail.com>
2016-02-20 19:37:53 -0800

    Use async function to access the database

--- server.js
+++ server.js
@@ -4,16 +4,16 @@ import GraphQLHTTP from 'express-graphql';
 import {MongoClient} from 'mongodb';
 
 let app = express();
+
 app.use(express.static('public'));
 
 (async () => {
   let db = await MongoClient.connect(process.env.MONGO_URL);
 
-  db = database;
-    app.use('/graphql', GraphQLHTTP({
-      schema: schema(db),
-      graphiql: true
-    }));
+  app.use('/graphql', GraphQLHTTP({
+    schema: schema(db),
+    graphiql: true
+  }));
 
-    app.listen(3000, () => console.log('Listening on port 3000'));
+  app.listen(3000, () => console.log('Listening on port 3000'));
 })();
