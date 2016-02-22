4aab2a2457508f78dd676212a9fdf9c179443a06
micahcourey <micahjcourey@gmail.com>
2016-02-21 18:57:41 -0800

    Add babel-relay-plugin

--- js/app.js
+++ js/app.js
@@ -1,6 +1,16 @@
 import React from "react";
 import ReactDom from "react-dom";
+import Relay from "react-relay";
 
 import Main from "./components/Main"
 
 ReactDom.render(<Main />, document.getElementById('react'));
+
+console.log(
+  Relay.QL`
+    query Test {
+      links {
+        title
+      }
+    }`
+);
